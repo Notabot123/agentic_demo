32ad4a9c60c476120caa4e1935929eaab8a4106b
Notabot123 <37666014+Notabot123@users.noreply.github.com>
2026-04-24 16:33:38 +0100

    try again with CORS debug

--- backend/app/main.py
+++ backend/app/main.py
@@ -31,8 +31,8 @@ app = FastAPI(title=settings.app_name, lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.frontend_origin, "http://localhost:5173", "http://127.0.0.1:5173"],
-    allow_credentials=True,
+    allow_origins=["*"],#[settings.frontend_origin, "http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
